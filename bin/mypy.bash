b118daeff695fdb97ce884350e51c0fe2026be0f
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-11-07 16:21:56 +0900

    Add CI settings

---
+++ bin/mypy.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mypy python_config/*.py || exit 1
+
+
+for testdir in $(find $(dirname $0)/../ -name test -and -type d)
+do
+    for d in $(find $testdir -type d -and -not -name __pycache__)
+    do
+        if ls $d/*.py 2> /dev/null > /dev/null
+        then
+            mypy --no-site-packages --ignore-missing-imports $d/*.py || exit 1
+        fi
+    done
+done
